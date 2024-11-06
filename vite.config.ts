e9da4b5cb6121aeac6099a5913f33fde0f584f5e
rtk5 <rithvik.matta@gmail.com>
2024-11-06 09:18:49 +0530

    nov 6

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // This allows access from the network
+    // port: 5173  // Optional: specify a custom port if needed
+  }
 });
